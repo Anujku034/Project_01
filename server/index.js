9d3a07b330b98d25cacc680125d79874ca1f8b2b
Anuj kumar <anujku603.@gmail.com>
2026-04-21 18:02:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://project-01-client.onrender.com",
     credentials:true
 }))
 
